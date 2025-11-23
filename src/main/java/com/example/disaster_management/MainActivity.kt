205be990084cf6966b0bc45741d75cfe0a18c5fc
Venky-234 <77890305+Venky-234@users.noreply.github.com>
2025-11-23 08:31:32 +0530

    Add files via upload
    Final Commit

--- src/main/java/com/example/disaster_management/MainActivity.kt
+++ src/main/java/com/example/disaster_management/MainActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.gms.location.LocationServices
 import androidx.activity.result.contract.ActivityResultContracts
 import android.location.Location
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.util.Log
@@ -22,15 +23,31 @@ import android.provider.ContactsContract
 import android.net.Uri
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.telephony.SmsManager
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    // ================ LOCATION ===============================
+    // ================ LOCATION (START) ===============================
     var LATITUDE = ""
     var LONGITUDE = ""
+
+    var recepient_phone_number = ""
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "contacts")
 
     private val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -70,8 +87,13 @@ class MainActivity : AppCompatActivity() {
                     LATITUDE = latitude.toString()
                     LONGITUDE = longitude.toString()
 
+                    for (i in 0 until myPhoneNumbers.size) {
+                        recepient_phone_number = myPhoneNumbers[i][0]
+                        sendSms()
+                    }
+
                     //coordinates_text = "Lat: $latitude, Lon: $longitude"
-                    Toast.makeText(this, "Location: $latitude, $longitude", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Location: $latitude, $longitude", Toast.LENGTH_SHORT).show()
 
                 } else {
 
@@ -86,10 +108,10 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Failed to get location.", Toast.LENGTH_SHORT).show()
             }
     }
-    // ================ LOCATION ===============================
+    // ================ LOCATION (END) ===============================
 
 
-    // ================ PHONE NUMBER LIST =========================
+    // ================ PHONE NUMBER LIST (START) =========================
     // 1. Create a list to store the numbers
     private val myPhoneNumbers = mutableListOf<List<String>>()
 
@@ -131,19 +153,29 @@ class MainActivity : AppCompatActivity() {
                 val cleanNumber = number.replace("[^0-9+]".toRegex(), "")
 
                 // Add to your list
-                myPhoneNumbers.add((listOf(name, cleanNumber)))
+                myPhoneNumbers.add((listOf(cleanNumber, name)))
+
+                val obj = object {
+                    val number = stringPreferencesKey(cleanNumber)
+                }
+                suspend fun saveUserName(context: Context, name: String) {
+                    context.dataStore.edit { preferences ->
+                        preferences[obj.number] = name
+                    }
+                }
+
+                lifecycleScope.launch{
+                    saveUserName(this@MainActivity, name)
+                }
 
                 val contacts_table = findViewById<TableLayout>(R.id.contacts_table)
                 val row = TableRow(this)
                 val txt_view = TextView(this)
-                txt_view.text = name + '\t' + cleanNumber
+                txt_view.text = cleanNumber + " = " + name
                 row.addView(txt_view)
                 contacts_table.addView(row)
 
-
-
                 findViewById<TextView>(R.id.textView).text = myPhoneNumbers.toString()
-
 
                 Log.d("ContactApp", "Number: $cleanNumber")
                 Log.d("ContactApp", "Name: $name")
@@ -161,7 +193,68 @@ class MainActivity : AppCompatActivity() {
         }
         contactPickerLauncher.launch(intent)
     }
-    // ================ PHONE NUMBER LIST =========================
+    // ================ PHONE NUMBER LIST (END) =========================
+
+
+    ///=============== SMS MANAGER (START) =======================
+    private val SMS_PERMISSION_CODE = 101
+
+    private fun checkAndRequestSmsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                SMS_PERMISSION_CODE
+            )
+        } else {
+            // Permission is already granted
+            //sendSms()
+        }
+    }
+
+    // Handle the result of the permission request
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                //sendSms()
+            } else {
+                // Permission denied
+                // Handle the denial, e.g., show a Toast or disable the SMS feature
+                Log.d("failed", "PERMISSION NOT GIVEN")
+            }
+        }
+    }
+
+    fun sendSms() {
+        val phoneNumber = recepient_phone_number // The recipient's phone number
+        var message = "PLEASE HELP ME , i am at this location \n latitude: " + LATITUDE + " longitude: " + LONGITUDE + "\n"
+        message += "https://maps.google.com/?q=" + LATITUDE + "," + LONGITUDE
+
+        try {
+            // Get the default SmsManager instance
+            // For API levels 23 and higher (Marshmallow+), use getSystemService
+            val smsManager: SmsManager = if (android.os.Build.VERSION.SDK_INT >= 23) {
+                this.getSystemService(SmsManager::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                SmsManager.getDefault()
+            }
+
+            // The last two arguments (PendingIntents) are optional but can be used
+            // to receive status updates (sent and delivered)
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+
+            Toast.makeText(applicationContext, "SMS sent successfully to " + phoneNumber + "!", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, "SMS sending failed: ${e.message}", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+    ///=============== SMS MANAGER (END) =======================
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -180,17 +273,38 @@ class MainActivity : AppCompatActivity() {
         val clear_contacts_button = findViewById<Button>(R.id.clear_contacts)
         val contacts_table = findViewById<TableLayout>(R.id.contacts_table)
 
+        suspend fun getAllKeys(context: Context) {
+            context.dataStore.data.first().asMap().forEach { (key, value) ->
+                myPhoneNumbers.add(listOf(key.name, value.toString()))
+                val row = TableRow(context)
+                val txtView = TextView(context)
+                txtView.text = "${key.name} + " = " + "{value}"
+                row.addView(txtView)
+                contacts_table.addView(row)
+            }
+        }
+        lifecycleScope.launch {
+            getAllKeys(this@MainActivity)
+        }
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        requestLocationPermission()
+        //requestLocationPermission()
 
         val text_field = findViewById<TextView>(R.id.textView)
 
+        checkAndRequestSmsPermission()
+
         emergency_button.setOnClickListener {
             //Toast.makeText(this, "Emergency button clicked", Toast.LENGTH_SHORT).show()
-            count += 1
 
             requestLocationPermission()
-            //text_field.text =  LATITUDE + " " + LONGITUDE + " " + count.toString()
+
+            /*
+            for (i in 0 until myPhoneNumbers.size) {
+                recepient_phone_number = myPhoneNumbers[i][0]
+                sendSms()
+            }
+            */
         }
 
 
@@ -200,10 +314,19 @@ class MainActivity : AppCompatActivity() {
             //text_field.text = myPhoneNumbers.toString()
         }
 
+        suspend fun clearDataStore(context: Context) {
+            context.dataStore.edit { preferences ->
+                preferences.clear()
+            }
+        }
         clear_contacts_button.setOnClickListener {
             myPhoneNumbers.clear()
-            text_field.text = myPhoneNumbers.toString()
+            //text_field.text = myPhoneNumbers.toString()
             contacts_table.removeAllViews()
+            lifecycleScope.launch {
+                clearDataStore(this@MainActivity)
+                Toast.makeText(this@MainActivity, "DataStore cleared!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
